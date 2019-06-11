a960a4dd7f38a96db2e4cdd8038a8c6039171638
spatten <scott@scottpatten.ca>
2019-06-11 12:31:06 -0700

    Adding a superscript parser

--- extensions/core-extensions.c
+++ extensions/core-extensions.c
@@ -1,6 +1,7 @@
 #include "cmark-gfm-core-extensions.h"
 #include "autolink.h"
 #include "strikethrough.h"
+#include "superscript.h"
 #include "table.h"
 #include "tagfilter.h"
 #include "tasklist.h"
@@ -11,6 +12,7 @@ static int core_extensions_registration(cmark_plugin *plugin) {
   cmark_plugin_register_syntax_extension(plugin, create_table_extension());
   cmark_plugin_register_syntax_extension(plugin,
                                          create_strikethrough_extension());
+  cmark_plugin_register_syntax_extension(plugin, create_superscript_extension());
   cmark_plugin_register_syntax_extension(plugin, create_autolink_extension());
   cmark_plugin_register_syntax_extension(plugin, create_tagfilter_extension());
   cmark_plugin_register_syntax_extension(plugin, create_tasklist_extension());
